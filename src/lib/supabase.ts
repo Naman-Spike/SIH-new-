4e968d2560e552d5132a65aa282e0adc41f06172
Your Name <2630btech1749@kiet.edu>
2026-09-04 16:42:22 +0530

    yes

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,3 +4,4 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://enmqwjldwpw
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVubXF3amxkd3B3cnZkdmhqcWpxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgxODIzNTAsImV4cCI6MjEwMzc1ODM1MH0.grKRs_cEpY9StENl9YogVKPSS3Pd8w9IQReQSc39TiU';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
